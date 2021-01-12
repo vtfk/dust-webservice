44e3120e0535aa51d17fe3346032b8d16c6e50e1
Rune Moskvil Lyngås <rune.moskvil.lyngaas@vtfk.no>
2021-01-12 09:50:47 +0100

    Disabled cache File invoke now runs sync(Promise)

--- index.js
+++ index.js
@@ -6,13 +6,22 @@ const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.set('etag', false); // disable cache
+
+const parseJson = str => {
+  try {
+    return JSON.parse(str);
+  } catch (error) {
+    return false;
+  }
+}
 
 app.get('/', (req, res) => {
   console.log('Hello world', req.ip);
   res.send('Poste ut en beskrivende oversikt over hva API\'en kan!');
 });
 
-app.get('/ps/file/invoke', (req, res) => {
+app.get('/invoke/psfile', (req, res) => {
   console.log(req.body);
 
   if (!req.body) {
@@ -29,9 +38,21 @@ app.get('/ps/file/invoke', (req, res) => {
     });
   }
 
-  const result = invokePSFile(req.body.filePath, req.body.args || undefined);
-  res.send(result);
-  console.log("After exec og res send");
+  invokePSFile(req.body.filePath, req.body.args || undefined)
+    .then(result => {
+      const json = parseJson(result);
+      json ? res.json(json) : res.send({
+        statusCode: 200,
+        message: result
+      });
+    })
+    .catch(error => {
+      const json = parseJson(error);
+      json ? res.status(500).json(json) : res.status(500).send({
+        statusCode: 500,
+        message: error
+      });
+    });
 });
 
 app.listen(config.EXPRESS_PORT, () => {
