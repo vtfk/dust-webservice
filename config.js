ccd1a16bdba6441314c78c6ed402d69f909cf527
Rune Moskvil Lyngås <rune.moskvil.lyngaas@vtfk.no>
2021-01-12 10:45:14 +0100

    Added max buffer to envs

--- config.js
+++ config.js
@@ -1,5 +1,6 @@
 require('dotenv').config()
 
 module.exports = {
-  EXPRESS_PORT: process.env.EXPRESS_PORT || 3000
+  EXPRESS_PORT: process.env.EXPRESS_PORT || 3000,
+  MAX_BUFFER: process.env.MAX_BUFFER || 1024*10000
 }
