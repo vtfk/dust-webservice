e1250775eebc8aa715140bb741b10d315372024e
Rune Moskvil Lyngås <rune.moskvil.lyngaas@vtfk.no>
2021-01-13 11:03:15 +0100

    Many changes... - Added dynamic endpoint for calling script name - Endpoints now correctly sets statusCode when error occurs - Endpoints validates filePath existens before trying to execute

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const invokePSFile = require('./lib/invoke-ps-file');
-const { EXPRESS_PORT } = require('./config');
+const config = require('./config');
 
+const { existsSync } = require('fs');
+const { join } = require('path');
 const express = require('express');
 const app = express();
 
@@ -16,27 +18,50 @@ const isValidJSON = str => {
   }
 }
 
+// default endpoint to list out what I can do
 app.get('/', (req, res) => {
-  console.log('Hello world', req.ip);
-  res.send('Poste ut en beskrivende oversikt over hva API\'en kan!');
+  console.log('Hello world', req.headers['x-forwarded-for'] || req.socket.remoteAddress);
+  res.send(`Poste ut en beskrivende oversikt over hva API\'en kan! ${req.headers['x-forwarded-for'] || req.socket.remoteAddress}`);
 });
 
-app.get('/invoke/psfile', (req, res) => {
+// endpoint for invoking scriptname in given service
+app.get('/:service/invoke', (req, res) => {
+  const caller = req.headers['x-forwarded-for'] || req.socket.remoteAddress;
+
   if (!req.body) {
-    return res.json({
+    return res.status(500).json({
       statusCode: 500,
-      message: "JSON input is required!"
+      message: `JSON input is required for endpoint '${req.params.service}'!`
     });
   }
 
-  if (!req.body.filePath) {
-    return res.json({
+  if (!req.body.fileName) {
+    return res.status(500).json({
       statusCode: 500,
-      message: "filePath is required"
+      message: `'fileName' is required for endpoint '${req.params.service}'!`
+    });
+  }
+  
+  const servicePath = config[`${req.params.service.toUpperCase()}_PATH`];
+  console.log(`${caller} ::`, 'Endpoint:', `'/${req.params.service}/invoke'`, 'ScriptPath:', `'${servicePath}'`);
+
+  if (!servicePath || !existsSync(servicePath)) {
+    return res.status(404).json({
+      statusCode: 404,
+      message: `'${req.params.service}' is not a valid script endpoint!`
     });
   }
 
-  invokePSFile(req.body.filePath, req.body.args || undefined)
+  const filePath = join(servicePath, req.body.fileName);
+
+  if (!existsSync(filePath)) {
+    return res.status(404).json({
+      statusCode: 404,
+      message: `'${req.body.fileName}' is not a valid script for endpoint '${req.params.service}'!`
+    });
+  }
+
+  invokePSFile(filePath, caller, req.body.args || undefined)
     .then(result => {
       const json = isValidJSON(result);
       json ? res.json(json) : res.send({
@@ -45,14 +70,59 @@ app.get('/invoke/psfile', (req, res) => {
       });
     })
     .catch(error => {
-      const json = isValidJSON(error);
+      console.log(`${caller} ::${error.stack}`);
+      const json = isValidJSON(error.message);
       json ? res.status(500).json(json) : res.status(500).send({
         statusCode: 500,
-        message: error
+        message: error.message
       });
     });
 });
 
-app.listen(Number.parseInt(EXPRESS_PORT), () => {
-  console.log(`DUST service listening on port ${EXPRESS_PORT}!`);
+// endpoint for invoking script by full path
+app.get('/invoke/psfile', (req, res) => {
+  const caller = req.headers['x-forwarded-for'] || req.socket.remoteAddress;
+
+  if (!req.body) {
+    return res.status(500).json({
+      statusCode: 500,
+      message: "JSON input is required!"
+    });
+  }
+
+  if (!req.body.filePath) {
+    return res.status(500).json({
+      statusCode: 500,
+      message: "'filePath' is required"
+    });
+  }
+
+  if (!existsSync(req.body.filePath)) {
+    return res.status(404).json({
+      statusCode: 404,
+      message: `'${req.body.filePath}' is not a valid script!`
+    });
+  }
+
+  invokePSFile(req.body.filePath, caller, req.body.args || undefined)
+    .then(result => {
+      const json = isValidJSON(result);
+      json ? res.json(json) : res.send({
+        statusCode: 200,
+        message: result
+      });
+    })
+    .catch(error => {
+      console.log(`${caller} :: ${error.stack}`)
+      const json = isValidJSON(error.message);
+      json ? res.status(500).json(json) : res.status(500).send({
+        statusCode: 500,
+        message: error.message
+      });
+    });
+});
+
+// start server on specified port
+app.listen(Number.parseInt(config.EXPRESS_PORT), () => {
+  console.log(`Script service listening on port ${config.EXPRESS_PORT}!`);
 });
