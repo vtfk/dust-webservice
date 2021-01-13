943e1d4a083c45038e984e8c6887b37c3531205e
Mats Andreassen <matsand@live.no>
2021-01-13 15:33:54 +0100

    Standard fix

--- config.js
+++ config.js
@@ -2,6 +2,6 @@ require('dotenv').config()
 
 module.exports = {
   EXPRESS_PORT: process.env.EXPRESS_PORT || 3000,
-  MAX_BUFFER: process.env.MAX_BUFFER || 1024*10000,
+  MAX_BUFFER: process.env.MAX_BUFFER || 1024 * 10000,
   DUST_PATH: process.env.DUST_PATH || '.'
 }
--- index.js
+++ index.js
@@ -1,26 +1,26 @@
-const invokePSFile = require('./lib/invoke-ps-file');
-const config = require('./config');
+const invokePSFile = require('./lib/invoke-ps-file')
+const config = require('./config')
 
-const { existsSync } = require('fs');
-const { join } = require('path');
-const express = require('express');
-const app = express();
+const { existsSync } = require('fs')
+const { join } = require('path')
+const express = require('express')
+const app = express()
 
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-app.set('etag', false); // disable cache
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+app.set('etag', false) // disable cache
 
 const isValidJSON = str => {
   try {
-    return JSON.parse(str);
+    return JSON.parse(str)
   } catch (error) {
-    return false;
+    return false
   }
 }
 
 // default endpoint to list out what I can do
 app.get('/', (req, res) => {
-  console.log('Hello world', req.headers['x-forwarded-for'] || req.socket.remoteAddress);
+  console.log('Hello world', req.headers['x-forwarded-for'] || req.socket.remoteAddress)
   res.send(`
 <h1>Usage</h1>
 
@@ -54,110 +54,110 @@ app.get('/', (req, res) => {
     ],
   }
 }
-</pre>`);
-});
+</pre>`)
+})
 
 // endpoint for invoking scriptname in given service
 app.get('/:service/invoke', (req, res) => {
-  const caller = req.headers['x-forwarded-for'] || req.socket.remoteAddress;
+  const caller = req.headers['x-forwarded-for'] || req.socket.remoteAddress
 
   if (!req.body) {
     return res.status(500).json({
       statusCode: 500,
       message: `JSON input is required for endpoint '${req.params.service}'!`
-    });
+    })
   }
 
   if (!req.body.fileName) {
-    console.log(`${caller} ::'fileName' is required for endpoint '${req.params.service}'`, req.body);
+    console.log(`${caller} ::'fileName' is required for endpoint '${req.params.service}'`, req.body)
     return res.status(500).json({
       statusCode: 500,
       message: `'fileName' is required for endpoint '${req.params.service}'!`,
       message2: req.params.body
-    });
+    })
   }
-  
-  const servicePath = config[`${req.params.service.toUpperCase()}_PATH`];
-  console.log(`${caller} ::`, 'Endpoint:', `'/${req.params.service}/invoke'`, 'ScriptPath:', `'${servicePath}'`);
+
+  const servicePath = config[`${req.params.service.toUpperCase()}_PATH`]
+  console.log(`${caller} ::`, 'Endpoint:', `'/${req.params.service}/invoke'`, 'ScriptPath:', `'${servicePath}'`)
 
   if (!servicePath || !existsSync(servicePath)) {
     return res.status(404).json({
       statusCode: 404,
       message: `'${req.params.service}' is not a valid script endpoint!`
-    });
+    })
   }
 
-  const filePath = join(servicePath, req.body.fileName);
+  const filePath = join(servicePath, req.body.fileName)
 
   if (!existsSync(filePath)) {
     return res.status(404).json({
       statusCode: 404,
       message: `'${req.body.fileName}' is not a valid script for endpoint '${req.params.service}'!`
-    });
+    })
   }
 
   invokePSFile(filePath, caller, req.body.args || undefined)
     .then(result => {
-      const json = isValidJSON(result);
+      const json = isValidJSON(result)
       json ? res.json(json) : res.send({
         statusCode: 200,
         message: result
-      });
+      })
     })
     .catch(error => {
-      console.log(`${caller} ::${error.stack}`);
-      const json = isValidJSON(error.message);
+      console.log(`${caller} ::${error.stack}`)
+      const json = isValidJSON(error.message)
       json ? res.status(500).json(json) : res.status(500).send({
         statusCode: 500,
         message: error.message
-      });
-    });
-});
+      })
+    })
+})
 
 // endpoint for invoking script by full path
 app.get('/invoke/psfile', (req, res) => {
-  const caller = req.headers['x-forwarded-for'] || req.socket.remoteAddress;
+  const caller = req.headers['x-forwarded-for'] || req.socket.remoteAddress
 
   if (!req.body) {
     return res.status(500).json({
       statusCode: 500,
-      message: "JSON input is required!"
-    });
+      message: 'JSON input is required!'
+    })
   }
 
   if (!req.body.filePath) {
     return res.status(500).json({
       statusCode: 500,
       message: "'filePath' is required"
-    });
+    })
   }
 
   if (!existsSync(req.body.filePath)) {
     return res.status(404).json({
       statusCode: 404,
       message: `'${req.body.filePath}' is not a valid script!`
-    });
+    })
   }
 
   invokePSFile(req.body.filePath, caller, req.body.args || undefined)
     .then(result => {
-      const json = isValidJSON(result);
+      const json = isValidJSON(result)
       json ? res.json(json) : res.send({
         statusCode: 200,
         message: result
-      });
+      })
     })
     .catch(error => {
       console.log(`${caller} :: ${error.stack}`)
-      const json = isValidJSON(error.message);
+      const json = isValidJSON(error.message)
       json ? res.status(500).json(json) : res.status(500).send({
         statusCode: 500,
         message: error.message
-      });
-    });
-});
+      })
+    })
+})
 
 // start server on specified port
 app.listen(Number.parseInt(config.EXPRESS_PORT), () => {
-  console.log(`Script service listening on port ${config.EXPRESS_PORT}!`);
-});
+  console.log(`Script service listening on port ${config.EXPRESS_PORT}!`)
+})
