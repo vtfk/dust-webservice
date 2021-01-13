442b80a155dc917dd6b1c1d8053a5c64aa13c053
Mats Andreassen <matsand@live.no>
2021-01-13 17:00:24 +0100

    La til logging ved henting av forsiden

--- handlers/frontpage.js
+++ handlers/frontpage.js
@@ -1,7 +1,11 @@
 var md = require('markdown-it')({ html: true })
+const { logger } = require('@vtfk/logger')
 const { readFileSync } = require('fs')
 
 module.exports = (req, res) => {
+  const caller = req.user || req.headers['x-forwarded-for'] || req.socket.remoteAddress
+  logger('debug', ['frontpage', caller, 'returning'])
+
   const usage = readFileSync('USAGE.md', 'utf-8')
   res.send(md.render(usage))
 }
