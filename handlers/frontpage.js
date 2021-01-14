27ccf90cf80dd18ac94b3a3f4be94f668b63b97b
Mats Andreassen <matsand@live.no>
2021-01-14 10:21:16 +0100

    JWT user prop er caller, ikke upn

--- handlers/frontpage.js
+++ handlers/frontpage.js
@@ -3,7 +3,7 @@ const { logger } = require('@vtfk/logger')
 const { readFileSync } = require('fs')
 
 module.exports = (req, res) => {
-  const caller = (req.user && req.user.upn) || req.headers['x-forwarded-for'] || req.socket.remoteAddress
+  const caller = (req.user && req.user.caller) || req.headers['x-forwarded-for'] || req.socket.remoteAddress
   logger('debug', ['frontpage', caller, 'returning'])
 
   const usage = readFileSync('USAGE.md', 'utf-8')
