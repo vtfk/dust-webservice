989321f01e08da811944966447f5af42386e2a88
Rune Moskvil Lyngås <rune.moskvil.lyngaas@vtfk.no>
2022-02-10 14:12:46 +0100

    Don't check jwt for favicon either

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const config = require('./config')
+const { JWT_SECRET, EXPRESS_PORT } = require('./config')
 const { logger } = require('@vtfk/logger')
 const express = require('express')
 const jwt = require('express-jwt')
@@ -10,8 +10,8 @@ app.use(express.urlencoded({ extended: true }))
 app.set('etag', false) // disable cache
 
 // JWT AUTH
-if (config.JWT_SECRET) {
-  app.use(jwt({ secret: process.env.JWT_SECRET, algorithms: ['HS256'] }).unless({ path: ['/'] }))
+if (JWT_SECRET) {
+  app.use(jwt({ secret: JWT_SECRET, algorithms: ['HS256'] }).unless({ path: ['/', '/favicon.ico'] }))
   app.use(require('./handlers/unauthorized'))
 }
 
@@ -21,6 +21,6 @@ app.get('/:service/invoke', require('./handlers/invoke-service')) // endpoint fo
 app.get('/invoke/psfile', require('./handlers/invoke-ps')) // endpoint for invoking script by full path
 
 // start server on specified port
-app.listen(Number.parseInt(config.EXPRESS_PORT), () => {
-  logger('info', `Script service listening on port ${config.EXPRESS_PORT}!`)
+app.listen(Number.parseInt(EXPRESS_PORT), () => {
+  logger('info', `Script service listening on port ${EXPRESS_PORT}!`)
 })
